3bd4bc11c4fe9bba0007b5d48387cfd9e1aa0761
ikbela <halilibela@gmail.com>
2022-09-01 10:28:16 +0200

    changes on sale repo

--- src/main/java/com/example/demo/repositories/PurchaseRepo.java
+++ src/main/java/com/example/demo/repositories/PurchaseRepo.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface PurchaseRepo extends CrudRepository<Long, Purchase> {
+public interface PurchaseRepo extends CrudRepository<Purchase,Long>{
 
     //List the purchases from last month
     @Query(value = "SELECT s FROM Sale WHERE DAY(NOW())-30<=DAY(s.createdAt)<DAY(NOW())",nativeQuery = true)
