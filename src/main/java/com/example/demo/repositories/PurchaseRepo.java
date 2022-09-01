1b5cda1ed0060427571de236de858d12dcba6362
Donatela Qesari <donatelaqesari@gmail.com>
2022-09-01 10:18:52 +0200

    PURCHASE Service COMMIT

--- src/main/java/com/example/demo/repositories/PurchaseRepo.java
+++ src/main/java/com/example/demo/repositories/PurchaseRepo.java
@@ -2,12 +2,13 @@ package com.example.demo.repositories;
 
 import com.example.demo.model.Purchase;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface PurchaseRepo {
+public interface PurchaseRepo extends CrudRepository<Long, Purchase> {
 
     //List the purchases from last month
     @Query(value = "SELECT s FROM Sale WHERE DAY(NOW())-30<=DAY(s.createdAt)<DAY(NOW())",nativeQuery = true)
