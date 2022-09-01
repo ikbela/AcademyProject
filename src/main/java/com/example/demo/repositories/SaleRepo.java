4530fcd0926c68df043e75e100d426bb4fb486fa
ikbela <halilibela@gmail.com>
2022-09-01 10:18:51 +0200

    changes on sale repo

--- src/main/java/com/example/demo/repositories/SaleRepo.java
+++ src/main/java/com/example/demo/repositories/SaleRepo.java
@@ -2,12 +2,13 @@ package com.example.demo.repositories;
 
 import com.example.demo.model.Sale;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface SaleRepo {
+public interface SaleRepo extends CrudRepository<Sale,Long> {
 
     @Query(value = "SELECT s FROM Sale WHERE DAY(NOW())-7<=DAY(s.createdAt)<DAY(NOW())",nativeQuery = true)
     List<Sale> allSalesFromLastWeek();
