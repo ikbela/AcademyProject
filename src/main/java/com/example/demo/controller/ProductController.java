e630f0488d435acbb210920e28f28bcf6591353a
AnxheloZuna <anxhelozuna@yahoo.com>
2022-09-01 12:52:23 +0200

    new controller and jsp and pom

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -2,26 +2,81 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Product;
 import com.example.demo.service.ProductService;
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Stream;
 
 @Controller
 public class ProductController {
     @Autowired
     private ProductService productService;
-    @GetMapping("/")
+    @GetMapping("/product")
     public String showProduct(Model model){
-        Product product= new Product("produkti07","Ska cna bo ne polecia",5000);
-        System.out.println(product.getPrice());
-        productService.addProduct(product);
         List<Product> allProducts= productService.getAllProducts();
         model.addAttribute("allProducts",allProducts);
         return "productPage.jsp";
 
     }
+    @GetMapping("/product/get-pdf")
+    public String getMinor10000Pdf(Model model){
+        List<Product> allProducts= productService.getAllProductMinor10000();
+        getPdf(allProducts);
+        model.addAttribute(productService.getAllProductMinor10000());
+        return "productPage.jsp";
+
+    }
+
+
+    public void getPdf(List<Product> productToAdd)  {
+        try {
+            Document document = new Document();
+            PdfWriter.getInstance(document, new FileOutputStream("Minor_1000_prod"+LocalDateTime.now()+".pdf"));
+            document.open();
+            PdfPTable table = new PdfPTable(4);
+            addTableHeaderProduct(table);
+            addRowsProduct(table,productToAdd);
+            document.add(table);
+            document.close();
+        }catch (Exception e){
+
+
+        }
+
+    }
+    private void addTableHeaderProduct(PdfPTable table) {
+        Stream.of("Name", "Discription", "Date", "Price")
+                .forEach(columnTitle -> {
+                    PdfPCell header = new PdfPCell();
+                    header.setBackgroundColor(BaseColor.LIGHT_GRAY);
+                    header.setBorderWidth(2);
+                    header.setPhrase(new Phrase(columnTitle));
+                    table.addCell(header);
+                });
+    }
+    private void addRowsProduct(PdfPTable table, List<Product> products) {
+        for (Product product:products) {
+            table.addCell(product.getName());
+            table.addCell(product.getDescription());
+            table.addCell( product.getCreatedAt().toString());
+            table.addCell(product.getPrice().toString());
+        }
+
+    }
+
 
 }
--- src/main/webapp/WEB-INF/productPage.jsp
+++ src/main/webapp/WEB-INF/productPage.jsp
@@ -12,17 +12,42 @@
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <html>
 <head>
-    <title>Sales Table</title>
+    <title>Products</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css " rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css " rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
 </head>
 <body>
 
+    <table class="table table-striped border border-dark">
+        <thead>
+        <tr>
+            <td class="text-center" colspan="4">Products minor 10000</td>
+        </tr>
+        <tr>
+            <th>Name</th>
+            <th>Discription</th>
+            <th>Date</th>
+            <th>Price</th>
+        </tr>
+        </thead>
+        <tbody>
+        <c:forEach items="${allProducts}" var="product">
+            <tr>
+
+                <td> ${product.name}</td>
+                <td>${product.description}</td>
+                <td><fmt:formatDate value="${product.createdAt}" pattern="yyyy-MM-dd" /></td>
+                <td>${product.price}</td>
+
+            </tr>
+        </c:forEach>
+        <a href="/product/get-pdf">Get .PDF file</a>
+        </tbody>
+    </table>
 
 
-    <c:forEach items="${allProducts}" var="product">
-        ${product.name}
-        ${product.description}
-        ${product.createdAt}
-        ${product.price}
-    </c:forEach>
+
+
 </body>
 </html>
