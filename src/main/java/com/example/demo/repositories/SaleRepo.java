7ee9e5525836f53558d66cbc48c0d65837b481d1
ikbela <halilibela@gmail.com>
2022-09-01 09:53:22 +0200

    Sale repo and service commit

--- src/main/java/com/example/demo/repositories/SaleRepo.java
+++ src/main/java/com/example/demo/repositories/SaleRepo.java
@@ -1,8 +1,15 @@
 package com.example.demo.repositories;
 
-
+import com.example.demo.model.Sale;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SaleRepo {
+
+    @Query(value = "SELECT s FROM Sale WHERE DAY(NOW())-7<=DAY(s.createdAt)<DAY(NOW())",nativeQuery = true)
+    List<Sale> allSalesFromLastWeek();
+
 }
--- src/main/java/com/example/demo/service/SaleService.java
+++ src/main/java/com/example/demo/service/SaleService.java
@@ -1,7 +1,21 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Sale;
+import com.example.demo.repositories.SaleRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SaleService {
+
+    @Autowired
+    private SaleRepo saleRepo;
+
+
+    public List<Sale> allLastWeekSales(){
+        return  saleRepo.allSalesFromLastWeek();
+    }
+
 }
