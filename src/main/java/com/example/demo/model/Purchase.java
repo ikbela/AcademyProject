1d5672f2d2737792d55431d488b6d32d6de124e6
Donatela Qesari <donatelaqesari@gmail.com>
2022-09-01 09:55:04 +0200

    PURCHASE COMMIT

--- src/main/java/com/example/demo/model/Purchase.java
+++ src/main/java/com/example/demo/model/Purchase.java
@@ -1,6 +1,11 @@
 package com.example.demo.model;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
+import java.util.Date;
 
 @Entity
 @Table(name="purchases")
@@ -8,4 +13,72 @@ public class Purchase {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Size(min=3, message="Description must be greater than 3 characters")
+    private String description;
+
+    @Column(updatable=false)
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date createdAt;
+
+    @PositiveOrZero
+    private Integer price;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
+    private Product product;
+
+    //CONSTRUCTOR
+    public Purchase(Long id, String description, Date createdAt, Integer price, Product product) {
+        this.id = id;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.price = price;
+        this.product = product;
+    }
+
+    public Purchase() {
+    }
+
+    //GETTERS AND SETTERS
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
 }
