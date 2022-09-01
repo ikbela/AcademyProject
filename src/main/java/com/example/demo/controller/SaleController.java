738156cbcab47941c5fbeb5a4464634d6a21414e
ikbela <halilibela@gmail.com>
2022-09-01 10:15:02 +0200

    Sale repo and service commit

--- src/main/java/com/example/demo/controller/SaleController.java
+++ src/main/java/com/example/demo/controller/SaleController.java
@@ -1,4 +1,21 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.SaleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class SaleController {
+@Autowired
+private SaleService saleService;
+    @GetMapping("/SalesPage")
+
+    public String salesHomePage(Model model){
+
+        model.addAttribute("allSalesOfLastWeek",saleService.allLastWeekSales());
+
+        return "index.jsp";
+    }
 }
